d5381eb969b9293dd446e132269ebe7cdc2b27a2
LuisaB <lbetancur72@gmail.com>
2023-10-22 20:05:11 -0500

    Funcionamiento de las secciones

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Cube from "../cube/cube";
 import Menu from "../menu/menu";
 import Slider from "react-slick";
@@ -6,23 +6,23 @@ import "../twitt/slide.scss";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./home.scss";
-import { Fab, Grid, Modal, Box } from "@mui/material";
-import Card from "@mui/material/Card";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
+import { Fab, Modal, Box, Button } from "@mui/material";
+import {Card, CardContent, CardMedia} from "@mui/material";
 import Typography from "@mui/material/Typography";
-import Photo27 from "../../assets/images/27.jpeg";
-import Photo28 from "../../assets/images/28.jpeg";
-import Photo29 from "../../assets/images/29.jpeg";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import Favorite from "@mui/icons-material/Favorite";
+import Photo27 from "../../assets/images/dalia.jpg";
+import Photo28 from "../../assets/images/oceano.jpg";
+import Photo29 from "../../assets/images/faro.jpg";
 import Photo30 from "../../assets/images/30.jpeg";
 
 const settingsRTL = {
   dots: true,
   infinite: true,
-  speed: 1590,
+  speed: 590,
   slidesToShow: 1,
   slidesToScroll: 1,
-  autoplaySpeed: 1590,
+  autoplaySpeed: 5590,
   autoplay: true,
   rtl: true,
   centerMode: true,
@@ -35,7 +35,8 @@ function Home() {
       Id: "1",
       title: "dalia",
       subtitle: "campo de dalias ",
-      description: "científicamente conocidas como Dahlia, son plantas herbáceas perennes que se caracterizan por sus atractivas y variadas flores. Estas flores son originarias de América Central y México",
+      description:
+        "científicamente conocidas como Dahlia, son plantas herbáceas perennes que se caracterizan por sus atractivas y variadas flores. Estas flores son originarias de América Central y México",
       Create_at: "11/10/23",
       avatar: Photo27,
     },
@@ -43,7 +44,8 @@ function Home() {
       Id: "2",
       title: "Oceano",
       subtitle: "Oceano atlantico ",
-      description: " es el segundo océano más grande del mundo, después del Océano Pacífico. Se extiende entre América al oeste y Europa y África al este,",
+      description:
+        " es el segundo océano más grande del mundo, después del Océano Pacífico. Se extiende entre América al oeste y Europa y África al este,",
       Create_at: "11/10/23",
       avatar: Photo28,
     },
@@ -51,37 +53,60 @@ function Home() {
       Id: "3",
       title: "Faro",
       subtitle: "Francia",
-      description: "Francia cuenta con una gran cantidad de faros impresionantes a lo largo de su extensa costa, cada uno con su propio carácter y belleza. Uno de los faros más icónicos y conocidos de Francia es el Faro de Cordouan.",
+      description:
+        "Francia cuenta con una gran cantidad de faros impresionantes a lo largo de su extensa costa, cada uno con su propio carácter y belleza. Uno de los faros más icónicos y conocidos de Francia es el Faro de Cordouan.",
       Create_at: "11/10/23",
       avatar: Photo29,
     },
     {
       Id: "4",
       title: "Atardecer",
-      subtitle: "Atardecer en las playas tijuana ",
-      description: "ubicadas en Baja California, México, son verdaderamente impresionantes y ofrecen una experiencia única para los visitantes",
+      subtitle: "Atardecer en las playas Tijuana ",
+      description:
+        "ubicadas en Baja California, México, son verdaderamente impresionantes y ofrecen una experiencia única para los visitantes",
       Create_at: "11/10/23",
       avatar: Photo30,
     },
   ];
 
   const [selectedSection, setSelectedSection] = useState(null);
-
-  const handleSectionClick = (sectionId) => {
-    setSelectedSection(sectionId);
-  };
-
+  const [favorites, setFavorites] = useState([]);
+  const [cart, setCart] = useState([]);
   const [selectedposts, setSelectedposts] = useState(null);
   const [modalOpen, setModalOpen] = useState(false);
 
-  const handleOpenModal = (post) => {
+  const handleOpenModal = (post, action) => {
     setSelectedposts(post);
     setModalOpen(true);
+    if (action === "Favorite") {
+      const isFavorite = favorites.some((item) => item.Id === post.Id);
+      if (!isFavorite) {
+        setFavorites([...favorites, post]);
+      }
+    }
+    if (action === "ShoppingCart") {
+      const isCartItem = cart.some((item) => item.Id === post.Id);
+      if (!isCartItem) {
+        setCart([...cart, post]);
+      }
+    }
   };
 
   const handleCloseModal = () => {
     setSelectedposts(null);
     setModalOpen(false);
+  };
+
+  const handleSectionClick = (sectionId) => {
+    setSelectedSection(sectionId);
+  };
+
+  const contactSectionRef = useRef(null);
+
+  const handleScrollToContact = () => {
+    if (contactSectionRef.current) {
+      contactSectionRef.current.scrollIntoView({ behavior: "smooth" });
+    }
   };
 
   return (
@@ -116,16 +141,20 @@ function Home() {
                           src={posts.avatar}
                           alt="green iguana"
                         />
-                        <CardContent>
+                        <CardContent className="CardContent">
                           <Typography gutterBottom variant="h5" component="div">
                             {posts.title}
                           </Typography>
                           <Typography gutterBottom variant="h5" component="div">
                             {posts.subtitle}
                           </Typography>
-                          <Typography gutterBottom variant="h5" component="div">
-                          <button className="button">Ver mas</button>
-                          </Typography>
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={() => handleOpenModal(posts)}
+                          >
+                            Ver mas
+                          </Button>
                         </CardContent>
                       </Card>
                     ))}
@@ -138,6 +167,7 @@ function Home() {
 
         <section
           id="Contact"
+          ref={contactSectionRef}
           className={`section-padding2 ${
             selectedSection === "Contact" ? "active" : ""
           }`}
@@ -146,14 +176,38 @@ function Home() {
             <div className="row">
               <div className="col-md-3 wow fadeInLeft delay-05s">
                 <div className="section-title">
-                  <a href="#Contact" target="_blank"></a>
                   <p className="sec-para"></p>
                 </div>
               </div>
-              <div className="col-md-9"></div>
+              <div className="col-md-9">
+                <Card>
+                  <CardContent className="CardContent">
+                    <Typography variant="h6">
+                      Mis Favoritos ({favorites.length})
+                    </Typography>
+                    <table className="Table-fav">
+                      <tbody>
+                        {favorites.map((fav) => (
+                          <tr key={fav.Id}>
+                            <td>
+                              <img
+                                src={fav.avatar}
+                                alt="Imagen de favorito"
+                                className="table-image"
+                              />
+                            </td>
+                            <td variant="h2">{fav.title}</td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </CardContent>
+                </Card>
+              </div>
             </div>
           </div>
         </section>
+
         <section
           id="Products"
           className={`section-padding3 ${
@@ -164,47 +218,76 @@ function Home() {
             <div className="row">
               <div className="col-md-3 wow fadeInLeft delay-05s">
                 <div className="section-title">
-                  <a href="#Contact" target="_blank"></a>
-
                   <p className="sec-para"></p>
                 </div>
               </div>
-              <div className="col-md-9"></div>
+              <div className="col-md-9">
+                <Card className="card-table">
+                  <CardContent className="CardContent">
+                    <Typography variant="h6">
+                      Mis Compras ({cart.length})
+                    </Typography>
+                    <table className="Table-fav">
+                      <tbody>
+                        {cart.map((fav) => (
+                          <tr key={fav.Id}>
+                            <td>
+                              <img
+                                src={fav.avatar}
+                                alt="Imagen de favorito"
+                                className="table-image"
+                              />
+                            </td>
+                            <td variant="h1">{fav.title}</td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </CardContent>
+                </Card>
+              </div>
             </div>
           </div>
         </section>
       </div>
 
       <Modal
+        className="modal-slide"
         open={modalOpen}
-        onClose={handleCloseModal}
+        onClose={() => handleCloseModal(selectedSection)}
         aria-labelledby="modal-title"
         aria-describedby="modal-description"
       >
-        <Box
-          sx={{
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            width: 250,
-            bgcolor: 'background.paper',
-            boxShadow: 24,
-            p: 4,
-          }}
-        >
+        <Box className="Box-slide">
           <img
-            src={selectedposts ? selectedposts.avatar : ''}
+            src={selectedposts ? selectedposts.avatar : ""}
             alt="Imagen seleccionada"
-            style={{ width: '100%', height: 'auto' }}
           />
           {selectedposts && (
             <div className="modal-service-info">
-              <p>{selectedposts.title}</p>
-              <p>{selectedposts.subtitle}</p>
+              <h1>{selectedposts.title}</h1>
+              <h3>{selectedposts.subtitle}</h3>
               <p>{selectedposts.description}</p>
             </div>
           )}
+          <div className="button-fav-group">
+            <Fab
+              color="primary"
+              aria-label="Favorite"
+              className="Favorite"
+              onClick={() => handleOpenModal(selectedposts, "Favorite")}
+            >
+              <Favorite />
+            </Fab>
+            <Fab
+              color="secondary"
+              aria-label="ShoppingCart"
+              className="ShoppingCart"
+              onClick={() => handleOpenModal(selectedposts, "ShoppingCart")}
+            >
+              <AddShoppingCartIcon />
+            </Fab>
+          </div>
         </Box>
       </Modal>
     </div>
