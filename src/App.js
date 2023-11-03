0bd8bb7f31e529380a24c93801740b1668eaf51a
LuisaB <lbetancur72@gmail.com>
2023-11-02 23:35:15 -0500

    configuracion pagina de politica y privacidad

--- src/App.js
+++ src/App.js
@@ -3,21 +3,17 @@ import './App.scss';
 import Cube from './components/cube/cube';
 import Menu from './components/menu/menu';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-
 import Home from './components/Home/home.js';
-
-
 import Facebook from './components/Face/facebook.js';
 import Instagram from './components/Insta/instagram.js';
 import Twitter from './components/twitt/twitter';
+import Politica from './components/PoliticayPrivacidad/Politica';
 
 function App() {
   
 
   return (
-    
-    <Router>
-      
+    <Router>   
       <div className="App">
         <Menu/>
       </div>
@@ -26,6 +22,7 @@ function App() {
           <Route path="/facebook" element={<Facebook />} />
           <Route path="/instagram" element={<Instagram />} />
           <Route path="/twitter" element={<Twitter />} />
+          <Route path="/politica" element={<Politica />} />
         </Routes>
     </Router>
   );
